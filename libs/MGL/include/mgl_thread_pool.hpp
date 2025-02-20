4ffffc552b1f5a619e7d7ca8702cd93e1ddb5a4b
AksuCode <parsaviherio@gmail.com>
2025-02-20 22:12:42 +0200

    Thread pool class done

--- libs/MGL/include/mgl_thread_pool.hpp
+++ libs/MGL/include/mgl_thread_pool.hpp
@@ -2,10 +2,27 @@
 #define MGL_THREAD_POOL_HPP
 
 #include <thread>
+#include <vector>
+#include <mutex>
+#include <queue>
+#include <functional>
+#include <condition_variable>
 
 class MGL_ThreadPool {
+    // Hardware thread count : unsigned int n = std::thread::hardware_concurrency();
     public:
+        MGL_ThreadPool(int num_threads = std::thread::hardware_concurrency());
+        ~MGL_ThreadPool();
+        void executeConcurrentTask(std::function<void()> task);
+        const int & getPoolSize();
+        int getTaskQueueLength();
     private:
+        const int num_threads_;
+        std::vector<std::thread> thread_pool_;
+        std::mutex task_queue_mutex_;
+        std::condition_variable cv_;
+        std::queue<std::function<void()>> task_queue_;
+        bool quit_ = false;
 };
 
 #endif
---
+++ libs/MGL/src/mgl_thread_pool.cpp
@@ -0,0 +1,59 @@
+#include "../include/mgl_thread_pool.hpp"
+
+MGL_ThreadPool::MGL_ThreadPool(int num_threads) : num_threads_(num_threads) {
+    for (int i = 0; i < num_threads_; i++) {
+        thread_pool_.emplace_back([this] {
+            while(true) {
+                std::function<void()> task;
+
+                // Curly braces are essential because they create a new scope. unique_lock frees the mutex when out of its scope.
+                // Took 2 hours of debugging. Never even thought that braces could matter in such a way. FUCK
+                {
+                    std::unique_lock lk(task_queue_mutex_);
+                    cv_.wait(lk, [this]{return !task_queue_.empty() || quit_;});
+
+                    if (quit_ && task_queue_.empty()) {
+                        return;
+                    }
+
+                    task = std::move(task_queue_.front());
+                    task_queue_.pop();
+                } // Unlocked here
+
+                task();
+            }
+        });
+    }
+}
+
+MGL_ThreadPool::~MGL_ThreadPool() {
+
+    // Curly braces are essential because they create a new scope. unique_lock frees the mutex when out of its scope.
+    // Took 2 hours of debugging. Never even thought that braces could matter in such a way. FUCK
+    {
+        std::unique_lock lk(task_queue_mutex_);
+        quit_ = true;
+    } // Unlocked here
+
+    cv_.notify_all();
+
+    for (auto it = thread_pool_.begin(); it != thread_pool_.end(); it++) {
+        (*it).join();
+    }
+}
+
+void MGL_ThreadPool::executeConcurrentTask(std::function<void()> task) {
+    {
+        std::unique_lock lk(task_queue_mutex_);
+        task_queue_.emplace(std::move(task));
+    }
+    cv_.notify_one();
+}
+
+const int & MGL_ThreadPool::getPoolSize() {
+    return num_threads_;
+}
+
+int MGL_ThreadPool::getTaskQueueLength() {
+    return task_queue_.size();
+}
